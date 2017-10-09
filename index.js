d85f8deb2983b693686258ebe3596668a67eb017
sebastian-meier <contact@sebastianmeier.eu>
2017-10-09 18:19:45 +0200

    jobUpdate bugfix

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ var queue = (function () {
   }
 
   module.jobUpdate = function (params) {
-    module.updateStat(params.job_id, params.type, params.state)
+    //module.updateStat(params.job_id, params.type, params.state)
   }
 
   module.jobDone = function (params) {
