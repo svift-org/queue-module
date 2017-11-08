40c3b523e9ae416111ceb837da17be2852008196
sebastian-meier <contact@sebastianmeier.eu>
2017-11-08 17:57:36 +0100

    testing

--- index.js
+++ index.js
@@ -100,6 +100,8 @@ var queue = (function () {
       console.log(err.message)
     }
 
+    console.log(result)
+
     let lastID = result.rows[0].id
 
     db.query("SELECT job_id FROM svift_queue WHERE id = $1", [lastID], function(err, result){
